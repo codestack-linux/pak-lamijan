d46ee2a6a13fa40345da6a814ced10256ea2537c
bagaspratama <bpratama369@gmail.com>
2026-03-04 15:42:44 +0700

    update

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -170,7 +170,7 @@ export default function Sidebar() {
               item={{
                 name: "Management Users",
                 icon: User,
-                href: "/settings/user",
+                href: "/user",
                 color: "text-slate-400",
               }}
               isActive={pathname === "/settings/users"}
